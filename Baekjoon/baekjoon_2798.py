3eba417aac4b205d8512dccfe6d79431cc00d973
iamjunhyeong <dddodo447@gmail.com>
2022-11-21 21:06:50 +0900

    first commit

---
+++ Baekjoon/baekjoon_2798.py
@@ -0,0 +1,16 @@
+# NO.2798 블랙잭
+def blackjack(li, n, k):
+    res = 0
+    for i in range(n-2):
+        for j in range(i+1,n-1):
+            for z in range(j+1,n):
+                sum = li[i] + li[j] + li[z]
+                if sum <= k and sum > res:
+                    res = sum
+    
+    return res
+
+n, k = map(int,input().split())
+li = list(map(int,input().split()))
+
+print(blackjack(li,n,k))
