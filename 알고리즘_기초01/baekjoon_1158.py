10ac27f9ad37b66513fbac040605032402dc83ac
iamjunhyeong <dddodo447@gmail.com>
2022-11-30 21:54:14 +0900

    요세푸스 문제

---
+++ 알고리즘_기초01/baekjoon_1158.py
@@ -0,0 +1,14 @@
+# No. 1158 요세푸스 문제
+import sys
+input = sys.stdin.readline
+
+n, k = map(int,input().split())
+li = [i for i in range(1,n+1)]
+st = []
+i = 0
+while li:
+    i = (i+(k-1)) % len(li)
+    print(i)
+    st.append(li.pop(i))
+    
+print("<%s>" % (", ".join(map(str,st))))
