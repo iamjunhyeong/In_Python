2ed828de66f7ae85af63b57f8e5bde77b5cf732e
iamjunhyeong <dddodo447@gmail.com>
2022-12-05 12:26:29 +0900

    ANS)에디터

---
+++ 알고리즘_기초01/자료구조/ANS)baekjoon_1406.py
@@ -0,0 +1,43 @@
+#NO.1406 에디터
+# ANS 1 
+from sys import stdin
+
+stk1 = list(stdin.readline().strip())
+stk2 = []
+n = int(input())
+for line in stdin :
+    if line[0] == 'L' :
+        if stk1 : stk2.append(stk1.pop())
+        else : continue
+    elif line[0] == 'D' :
+        if stk2 : stk1.append(stk2.pop())
+        else : continue
+    elif line[0] == 'B' :
+        if stk1 : stk1.pop()
+        else : continue
+    elif line[0] == 'P' :
+        stk1.append(line[2])
+print(''.join(stk1 + list(reversed(stk2))))
+
+# ANS 2
+import sys
+input = sys.stdin.readline
+s = list(input().rstrip())
+c = []
+
+for _ in range(int(input())):
+    command = input().rstrip()
+    if command == "L":
+        if s:
+            c.append(s.pop())
+    elif command == "D":
+        if c:
+            s.append(c.pop())
+    elif command == "B":
+        if s:
+            s.pop()
+    else:
+        _,b = command.split()
+        s.append(b)
+s += c[::-1]
+print(*s,sep="")
