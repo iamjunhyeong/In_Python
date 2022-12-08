4e3607e3331d14f4c827da7fa455ded8dad252f1
iamjunhyeong <dddodo447@gmail.com>
2022-12-08 23:23:24 +0900

    문자열 분석

--- 알고리즘_기초01/자료구조/baekjooon_10820.py
+++ 알고리즘_기초01/자료구조/baekjooon_10820.py
@@ -1,18 +1,24 @@
+# NO.10820 문자열 분석
 while True:
-    dic = {1:0,2:0,3:0,4:0}
-    s = input()
-    for i in s:
-        if ord(i) > 96 and ord(i) < 123:
-            dic[1] += 1
-        elif ord(i) > 64 and ord(i) < 91:
-            dic[2] += 1
-        elif ord(i) > 47 and ord(i) < 58:
-            dic[3] += 1
-        else:
-            dic[4] += 1
+    try:
+        dic = {'lower':0,'upper':0,'num':0,'blank':0}
+        s = input()
+        for i in s:
+            if ord(i) > 96 and ord(i) < 123:
+                dic['lower'] += 1
+            elif ord(i) > 64 and ord(i) < 91:
+                dic['upper'] += 1
+            elif ord(i) > 47 and ord(i) < 58:
+                dic['num'] += 1
+            else:
+                dic['blank'] += 1
 
-    for i in dic:
-        print(dic[i], end=' ')
-    print()
+        for i in dic:
+            print(dic[i], end=' ')
+        print()
+    except EOFError:
+        break
 
-# 소문자 대문자 숫자 공백
+# 보통 문제에서 입력값의 개수, 즉 T(테스트케이스)가 주어지는데 
+# 이 문제는 안 주어짐을 깨닫고 while문으로 무한 루프를 돌리며 
+# EOFError인 상황만 제외하고는 반복문이 돌아가도록 하였다.
