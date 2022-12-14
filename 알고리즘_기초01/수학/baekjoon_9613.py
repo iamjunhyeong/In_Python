a235f628c65dd3cdfb169c34f708d6da301f9a42
iamjunhyeong <dddodo447@gmail.com>
2022-12-14 20:31:16 +0900

    GCD 합

---
+++ 알고리즘_기초01/수학/baekjoon_9613.py
@@ -0,0 +1,20 @@
+from itertools import combinations
+
+def gcd(x, y):
+    if y==0:
+        return x
+    else:
+        return gcd(y,x%y)
+
+n = int(input())
+li = []
+for _ in range(n):
+    res = 0
+    li = list(map(int,input().split()))
+    li = li[::-1]
+    li.pop()
+    ncr = combinations(li,2)
+
+    for i in ncr:
+        res += gcd(i[0],i[1])
+    print(res)
