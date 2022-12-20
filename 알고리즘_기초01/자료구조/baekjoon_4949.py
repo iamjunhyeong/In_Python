3da95c19680e601a38c24c5c5217cc3d31a709e0
iamjunhyeong <dddodo447@gmail.com>
2022-12-21 01:53:05 +0900

    균형잡힌세상

---
+++ 알고리즘_기초01/자료구조/baekjoon_4949.py
@@ -0,0 +1,24 @@
+import sys
+input = sys.stdin.readline
+
+while True:
+    a = input()
+    if a[0] == '.': break
+
+    st = []
+    for i in a:
+        if i == '(' or i == '[':
+            st.append(i)
+        elif i == ')':
+            if not st or st[-1] == '[':
+                break
+            st.pop(-1)
+        elif i == ']':
+            if not st or st[-1] == '(':
+                break
+            st.pop(-1)
+
+    if st or not i == '\n':
+        print("no")
+    else:
+        print("yes")
