74511a914291f430487621c12188fc2ded8b8122
iamjunhyeong <dddodo447@gmail.com>
2022-12-27 13:17:29 +0900

    개미전사

---
+++ 이코테/다이나믹 프로그래밍/개미 전사.py
@@ -0,0 +1,10 @@
+n = int(input())
+arr = list(map(int,input().split()))
+d = [0] * 100
+d[0] = arr[0]
+d[1] = arr[1]
+for i in range(2, len(arr)):
+    d[i] = max(d[i-1],d[i-2]+arr[i])
+
+print(d[n-1])
+
