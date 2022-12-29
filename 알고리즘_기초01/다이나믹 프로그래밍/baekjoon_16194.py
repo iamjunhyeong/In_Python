21f25b1d865112bbb41eb0f018384081365b146b
iamjunhyeong <dddodo447@gmail.com>
2022-12-29 17:57:47 +0900

    카드구매하기2

---
+++ 알고리즘_기초01/다이나믹 프로그래밍/baekjoon_16194.py
@@ -0,0 +1,16 @@
+#NO.11052 카드구매하기
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+cards = [0] + list(map(int,input().split()))
+d = [0 for _ in range(n+1)]
+
+for i in range(1,n+1):
+    d[i] = cards[i]
+    for k in range(1,i+1):
+        t = d[k] + cards[i-k]
+        if t < d[i]:
+            d[i] = t
+
+print(d[n])
