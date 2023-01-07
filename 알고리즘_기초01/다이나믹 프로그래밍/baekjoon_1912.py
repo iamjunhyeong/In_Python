f0d867f47715c2bb5a2eb1089e8402d44eb0eab0
iamjunhyeong <dddodo447@gmail.com>
2023-01-07 21:08:17 +0900

    연속합

--- 알고리즘_기초01/다이나믹 프로그래밍/baekjoon_1912.py
+++ 알고리즘_기초01/다이나믹 프로그래밍/baekjoon_1912.py
@@ -1,17 +1,8 @@
+#NO.1912 연속합
 n = int(input())
 arr = list(map(int,input().split()))
 
-dp = [0 for _ in range(n)]
-dp[0] = arr[0]
+for i in range(1, n):
+    arr[i] = max(arr[i], arr[i-1] + arr[i])
 
-for i in range(n-1):
-    dp[i] = arr[i]
-    for j in range(i+1,n):
-        if arr[j] > 0:
-            dp[j] = max(dp[j-1]+arr[j],arr[j])
-        if dp[j] < 0: break
-
-
-
-
-print(dp[n-1])
+print(max(arr))
