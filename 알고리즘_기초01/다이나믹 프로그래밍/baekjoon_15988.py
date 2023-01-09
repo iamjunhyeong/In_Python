9d822eb06e54d244d9283760e917ffe7f401e9b2
iamjunhyeong <dddodo447@gmail.com>
2023-01-09 18:14:42 +0900

    1,2,3 더하기 3

--- 알고리즘_기초01/다이나믹 프로그래밍/baekjoon_15988.py
+++ 알고리즘_기초01/다이나믹 프로그래밍/baekjoon_15988.py
@@ -1,10 +1,10 @@
+import sys
+input = sys.stdin.readline
+
+dp = [1,2,4,7]
 for _ in range(int(input())):
     n = int(input())
-    dp = [0 for _ in range(n+1)]
-    dp[1] = 1
-    dp[2] = 2
-    dp[3] = 4
-    for i in range(4,n+1):
-        dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
+    for i in range(len(dp),n):
+        dp.append((dp[-1] + dp[-2] + dp[-3])%1000000009)
 
-    print(dp[n])
+    print(dp[n-1])
