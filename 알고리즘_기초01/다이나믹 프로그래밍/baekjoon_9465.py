a3a3eb56027b63ed420ace627deee5c785887798
iamjunhyeong <dddodo447@gmail.com>
2023-01-13 14:23:08 +0900

    스티커

---
+++ 알고리즘_기초01/다이나믹 프로그래밍/baekjoon_9465.py
@@ -0,0 +1,16 @@
+#NO.9465 스티커
+import sys
+input = sys.stdin.readline
+
+for _ in range(int(input())):
+    n = int(input())
+    arr = []
+    for _ in range(2):
+        arr.append(list(map(int,input().split())))
+    arr[0][1] += arr[1][0]
+    arr[1][1] += arr[0][0]
+    for i in range(2,n):
+        arr[0][i] += max(arr[1][i-1],arr[1][i-2])
+        arr[1][i] += max(arr[0][i-1],arr[0][i-2])
+
+    print(max(arr[0][n-1], arr[1][n-1]))
