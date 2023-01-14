da5a25779edc0e3da130b28d5b9cc3a91808e9f1
iamjunhyeong <dddodo447@gmail.com>
2023-01-15 00:52:39 +0900

    가장 큰 증가 부분수열

---
+++ 알고리즘_기초01/다이나믹 프로그래밍/baekjoon_11055.py
@@ -0,0 +1,10 @@
+#NO.11055 가장 큰 증가 부분 수열
+n = int(input())
+arr = list(map(int,input().split()))
+dp = arr[:]
+print(dp)
+for i in range(n):
+    for j in range(i):
+        if arr[i] > arr[j]:
+            dp[i] = max(dp[i],dp[j]+arr[i])
+print(max(dp))
