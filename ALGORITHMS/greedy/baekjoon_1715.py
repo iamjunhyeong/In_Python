0876675e50e510bae02cd6e542504e306de1766f
iamjunhyeong <dddodo447@gmail.com>
2023-02-15 22:29:07 +0900

    카드정렬하기

--- ALGORITHMS/greedy/baekjoon_1715.py
+++ ALGORITHMS/greedy/baekjoon_1715.py
@@ -5,12 +5,14 @@ input = sys.stdin.readline
 
 n = int(input())
 li = []
+# heapq으로 입력받기
 for _ in range(n):
     num = int(input())
     heapq.heappush(li, num)
 res = 0
 
 while len(li) > 1:
+    # 최솟값 하나씩 빼고 더함
     a = heapq.heappop(li)
     b = heapq.heappop(li)
     res += a+b
